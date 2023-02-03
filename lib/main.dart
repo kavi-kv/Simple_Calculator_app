65bc59afba3413c0205eb2ef0f04d94e83688c4e
Kavi <kavialin23@gmail.com>
2023-02-03 07:32:50 +0300

    Updated to perform different math operations

--- lib/main.dart
+++ lib/main.dart
@@ -33,24 +33,42 @@ class _HomeState extends State<Home> {
             Padding(
               padding: const EdgeInsets.only(right: 10, left: 10),
               child: SizedBox(
-                width: 180,
+                width: 160,
                 child: TextField(
                   controller: _leftTextController,
                   decoration: InputDecoration(
-                    hintText: 'What\'s on your min',
+                    hintText: 'Enter a numbber',
                     border: OutlineInputBorder(),
                   ),
                 ),
               ),
             ),
+            Spacer(),
+            DropdownButton<String>(
+              onChanged: (String? newValue){
+                setState(() {
+                  selectedItems = newValue!;
+                });
+              },
+                value: selectedItems,
+                items: symbols.map<DropdownMenuItem<String>>(
+                  (String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value,style: TextStyle(fontSize: 22.0),),
+                    );
+                  },
+                ).toList(),
+              ),
+              Spacer(),
             Padding(
               padding: const EdgeInsets.only(right: 10, left: 10),
               child: SizedBox(
-                width: 180,
+                width: 160,
                 child: TextField(
                   controller: _rightTextController,
                   decoration: InputDecoration(
-                    hintText: 'What\'s on your min',
+                    hintText: 'Enter a numbber',
                     border: OutlineInputBorder(),
                   ),
                 ),
@@ -64,14 +82,18 @@ class _HomeState extends State<Home> {
         ElevatedButton(
             onPressed: () {
               setState(() {
-                int num1 = int.parse(_rightTextController.text);
-                int num2 = int.parse(_leftTextController.text);
-                total = num1+num2;
+                int num1 = int.parse(_leftTextController.text);
+                int num2 = int.parse(_rightTextController.text);
+                //total = num1 + num2;
+                total = Calculate(num1,num2,selectedItems!);
               });
             },
-            child: Text(
-              'Calculate',
-              style: TextStyle(fontSize: 20),
+            child: SizedBox(height: 50,width: 90,
+              child: Text(
+                'Calculate',
+                textAlign: TextAlign.end,
+                style: TextStyle(fontSize: 20),
+              ),
             )),
         SizedBox(
           height: 10,
@@ -83,4 +105,27 @@ class _HomeState extends State<Home> {
       ]),
     );
   }
+}
+
+int Calculate(int num1,int num2,String symbol)
+{
+  int result = 0;
+  if(symbol == '+')
+  {
+    return result = num1 + num2;
+  }
+  else if(symbol == '-')
+  {
+    return result = num1 - num2;
+  }
+  else if(symbol == '*')
+  {
+    return result = num1 * num2;
+  }
+  else if(symbol == '/')
+  {
+    return result = num1 ~/ num2;
+  }
+
+  return 0;
 }
